8d6edc589192765ff2ce444145026bfd01c82bcb
Xible <s156357@gmail.com>
2017-06-19 16:40:30 +0800

    add hello Selenium program

---
+++ Hello_Selenium/hello_selenium.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+'''
+firefox 47版本後需要下載第3方webdriver 'geckodriver'，並放到firefox資料夾中
+之後再將該路徑添加到環境變數path中
+參考網址:https://www.zhihu.com/question/49568096
+'''
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+driver.get("https://www.google.com")
+
+driver.find_element_by_id("gs_htif0").send_keys("Selenium2")
+driver.find_element_by_xpath(".//*[@id='tsf']/div[2]/div[3]/center/input[1]").click()
+
+driver.quit()
